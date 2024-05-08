c8f4fec0fdda1089aa9885fa292c5990e352599b
Klim <pportmanviolet@gmail.com>
2024-05-08 15:11:43 +0300

    logging

--- main.py
+++ main.py
@@ -6,9 +6,10 @@ from converters.xlsx_to_text import xlsx_to_text
 from converters.odt_to_text import odt_to_text
 import os
 import g4f
+import logging
 
-file_path = r'test_files\test.txt'
-theme = 'Япония 21 века'
+#file_path = r'test_files\test.txt'
+#theme = 'Япония 21 века'
 
 #словарь со всеми функциями конвертерами
 converters = {
@@ -21,6 +22,15 @@ converters = {
     '.odt': odt_to_text
 }
 
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+handler = logging.FileHandler(f"{__name__}.log", mode='a')
+formatter = logging.Formatter("%(name)s %(asctime)s %(levelname)s %(message)s")
+
+handler.setFormatter(formatter)
+logger.addHandler(handler)
+
 def ask_bot(text: str) -> None:
     '''
     takes text
@@ -32,22 +42,27 @@ def ask_bot(text: str) -> None:
         messages=[{'role': 'user', 'content': text}],
     )
 
-    print(response)
+    if response:
+        logger.info(f'ChatGPT response: {response}')
+    else:
+        logger.error(f'No response from ChatGPT')
 
 def get_text(file_path: str) -> None:
     '''
     takes file_path
     return None
     '''
-
-    _, file_extension = os.path.splitext(file_path)
+    try:
+        _, file_extension = os.path.splitext(file_path)
+    except:
+        logger.error(f'Incorrect file path')
 
     if file_extension in converters:
         return converters[file_extension](file_path)
     else:
-        print('Unsupported file extension')
+        logger.error(f'Unsupported file extension')
 
-def get_request() -> str:
+def get_request(theme, file_path) -> str:
     '''
     takes None
     return: str
@@ -61,6 +76,10 @@ def get_request() -> str:
 
 #вызов главной функции
 if __name__ == '__main__':
-    message = get_request()
+    theme = input('Type your theme to work with: ')
+    file_path = input('Please copy the file path: ')
+    file_path.encode('unicode_escape')
+
+    message = get_request(theme, file_path)
 
     ask_bot(message)
---
+++ saas.py
@@ -0,0 +1,4 @@
+
+file_path = input()
+file_path.encode('unicode_escape')
+print(file_path)
